44ae959dff8e26813debd5e1541a1a8ac4c5f5c8
213Leon213 <leon213@live.de>
2025-09-17 16:19:59 +0200

    hover effect added

--- template.js
+++ template.js
@@ -2,7 +2,7 @@
 function pokeTemplate(index) {
     return `<div class="card margin-10 text-nowrap-center bg-danger text-light border-warning" onclick="showPokemon(${index})" style="width: 12rem;">
     <h2>${pokeObj[index].name}</h2>
-  <img src="${pokeObj[index].sprites.other.home.front_default}" class="card-img-top ${getBgColor(index)}" alt="...">
+  <img src="${pokeObj[index].sprites.other.home.front_default}" class="card-img-top ${getBgColor(index)}">
   <div class="card-body column">
     <img src="${addPictureFirst(index)}" class="card-img-top margin-10" >
     <${imageCheck(index)}>
